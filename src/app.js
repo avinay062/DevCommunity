866ae721c77ab28281f8064221524d9c0913f98f
avinay062 <kumaravinay062@gmail.com>
2024-12-19 13:31:34 +0530

    Exploring get, post calls

--- src/app.js
+++ src/app.js
@@ -3,14 +3,33 @@ const express = require("express");
 const app = express(); // instance of an express js application
 
 //Request handeler
-app.use((req, res)=>{
-    res.send("Hello From the serverr !")
+app.use("/hello/1",(req, res)=>{
+    res.send("Hello From hello!")
 });
 
+//get call
+app.get("/test",(req, res)=>{
+    res.send("data fetched successfully !")
+});
+
+// POST call
+
+app.post("/test",(req, res)=>{
+    console.log("saving data to db");
+    res.send("data saved successfully !")
+});
+
+
 //request handeler for test route
-// app.use("/test",(req, res)=>{
-//     res.send("Hello From the server !")
-// });
+// this will match all HTTP methods
+app.use("/test",(req, res)=>{
+    res.send("Hello From the Test !")
+});
+
+//wildcard route
+app.use("/",(req, res)=>{
+    res.send("Hello Avinay !")
+});
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000 !")
