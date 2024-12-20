2c393d998998ea4448f2065180f9d130c8997303
avinay062 <kumaravinay062@gmail.com>
2024-12-20 15:14:06 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const connectDB = require("./config/database");
 
 const User = require("./models/user");
 
-app.use(express.json());
+app.use(express.json()); // middleware by express.js for reading the data and parsing it to json
+
+//POST API for signup
 app.post("/signup", async(req,res)=>{
     const user = new User(req.body);
    try{
@@ -16,6 +18,54 @@ app.post("/signup", async(req,res)=>{
       res.status(400).send("Error Saving the User:" + err.message);
    }  
 });
+
+//get user by Email id
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body?.emailId;
+    try{
+        const users = await User.find({emailId : userEmail});
+        if(users.length === 0){
+            res.status(406).send("user not found");
+        } else {
+            res.send(users);
+        }
+    } 
+    catch(err){
+        res.status(400).send("Error Getting the User detail");
+    }   
+});
+
+//get user by Email id and find one documentt
+// app.get("/user", async(req,res)=>{
+//     const userEmail = req.body?.emailId;
+//     try{
+//         const users = await User.findOne({emailId : userEmail});
+//         if(users.length === 0){
+//             res.status(406).send("user not found");
+//         } else {
+//             res.send(users);
+//         }
+//     } 
+//     catch(err){
+//         res.status(400).send("Error Getting the User detail");
+//     }   
+// });
+
+//get All Document
+app.get("/feed", async(req,res)=>{
+    try{
+        const users = await User.find({});
+        if(users.length === 0){
+            res.status(406).send("user not found");
+        } else {
+            res.send(users);
+        }
+    } 
+    catch(err){
+        res.status(400).send("Error Getting the User detail");
+    }   
+});
+
 
 
 connectDB().then(()=>{
