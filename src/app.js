3f0831b49a51496c602e5ad068ec99513e184d12
avinay062 <kumaravinay062@gmail.com>
2024-12-20 14:32:38 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ const connectDB = require("./config/database");
 
 const User = require("./models/user");
 
+app.use(express.json());
+app.post("/signup", async(req,res)=>{
+    const user = new User(req.body);
+   try{
+    await user.save();
+    res.send("User is Added Successfully..!");
+   } catch(err) {
+      res.status(400).send("Error Saving the User:" + err.message);
+   }  
+});
+
+
 connectDB().then(()=>{
     console.log("Database connected established..!");
     //to listen the server
@@ -17,19 +29,8 @@ connectDB().then(()=>{
     console.log("Database cannot connected..!")
 });
 
-app.post("/signup", async(req,res)=>{
-    const user = new User({
-        firstName: "Virat",
-        lastName: "Kohli",
-        emailId: "virat.kohli@gmail.com",
-        password: "virat@123"
-    });
-   try{
-    await user.save();
-    res.send("User is Added Successfully..!");
-   } catch(err) {
-      res.status(400).send("Error Saving the User:" + err.message);
-   }  
-});
+
+
+
 
 
