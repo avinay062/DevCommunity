7103c08ca669bc1ff1df1db0ed1fba59742a56ed
that-devops-guy <aroopkumargochhayat@gmail.com>
2025-03-24 00:01:08 +0530

    Update mongo connection string

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() =>{
-    await mongoose.connect("mongodb+srv://avinay_node:11RCV7RUUTe7f7zT@node.m1eax.mongodb.net/devCommunity");
+    await mongoose.connect("mongodb://admin:devcompass@54.89.250.108:27017/devCommunity");
 };
 
 module.exports = connectDB;
