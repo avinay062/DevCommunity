dfcce265278cc5addd209a9f12e35696ab318b6d
avinay062 <kumaravinay062@gmail.com>
2025-03-24 01:05:24 +0530

    Mongo db connection string change

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const connectDB = async() =>{
-    await mongoose.connect("mongodb://54.89.250.108:27017/devCommunity");
+const connectDB = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
 };
 
 module.exports = connectDB;
